04fada36c955643ffd04bfef751a4b22f98548a6
ЭМИИА ИИ <vstarostin@emiia.ru>
2022-08-25 23:27:19 +0300

    Update map-gl-indoor.umd.js

--- json/map-gl-indoor.umd.js
+++ json/map-gl-indoor.umd.js
@@ -593,7 +593,7 @@
     		source: "indoor",
     		paint: {
     			"line-color": "#6B6B69",
-    			"line-width": 8,
+    			"line-width": 5,
     			"line-opacity": {
     				base: 1,
     				stops: [
