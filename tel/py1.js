cbe7f930b4d2de85d8ba758be6fe2809b5a71e3e
ЭМИИА ИИ <vstarostin@emiia.ru>
2023-03-08 15:55:12 +0300

    Update py1.js

--- tel/py1.js
+++ tel/py1.js
@@ -1,29 +1,40 @@
 from telebot import types
 import telebot
 
-bot = telebot.TeleBot('bottoken')
-
-ddef webAppKeyboard(chat_id):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    webAppBim = types.WebAppInfo("https://www.emiia.ru/osm/indoor9.html#18.31/55.977272/37.17427/62.4/45")
-    webAppEdm = types.WebAppInfo("https://games.mihailgok.ru")
-    webAppCloud = types.WebAppInfo("https://www.emiia.ru/osm/indoor9.html#18.31/55.977272/37.17427/62.4/45")
-    webAppData = types.WebAppInfo("https://games.mihailgok.ru")
-    one_butt = types.KeyboardButton(text="ТИМ (BIM)", web_app=webAppBim)
-    two_butt = types.KeyboardButton(text="ЭДО (EDM)", web_app=webAppEdm)
-    three_butt = types.KeyboardButton(text="CLOUD EMIIA.AI", web_app=webAppCloud)
-    four_butt = types.KeyboardButton(text="DATA ANALYSIS", web_app=webAppData)
-    keyboard.add(one_butt, two_butt)
-    keyboard.add(three_butt, four_butt)
-    return keyboard
+bot = telebot.TeleBot('')
 
 
-def webAppKeyboardInline():
-    keyboard = types.InlineKeyboardMarkup(row_width=1)
-    webApp = types.WebAppInfo("https://telegram.mihailgok.ru")
-    one = types.InlineKeyboardButton(text="Веб приложение", web_app=webApp)
-    keyboard.add(one)
-    return keyboard
+
+
+def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
+   keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) #создаем клавиатуру
+
+   webAppBim = types.WebAppInfo("https://games.mihailgok.ru")
+   webAppEdm = types.WebAppInfo("https://games.mihailgok.ru")
+   webAppCloud = types.WebAppInfo("https://www.emiia.ru/osm/indoor9.html#18.31/55.977272/37.17427/62.4/45")
+   webAppData = types.WebAppInfo("https://games.mihailgok.ru")
+   one_butt = types.KeyboardButton(text="ТИМ (BIM)", web_app=webAppBim)
+   two_butt = types.KeyboardButton(text="ЭДО (EDM)", web_app=webAppEdm)
+   three_butt = types.KeyboardButton(text="CLOUD EMIIA.AI", web_app=webAppCloud)
+   four_butt = types.KeyboardButton(text="DATA ANALYSIS", web_app=webAppData)
+   keyboard.add(one_butt, two_butt)
+   keyboard.add(three_butt, four_butt)
+
+   return keyboard #возвращаем клавиатуру
+
+
+
+
+
+
+
+def webAppKeyboardInline(): #создание inline-клавиатуры с webapp кнопкой
+   keyboard = types.InlineKeyboardMarkup(row_width=1) #создаем клавиатуру inline
+   webApp = types.WebAppInfo("https://telegram.mihailgok.ru") #создаем webappinfo - формат хранения url
+   one = types.InlineKeyboardButton(text="Веб приложение", web_app=webApp) #создаем кнопку типа webapp
+   keyboard.add(one) #добавляем кнопку в клавиатуру
+
+   return keyboard #возвращаем клавиатуру
 
 
 @bot.message_handler(commands=['start']) #обрабатываем команду старт
@@ -36,12 +47,12 @@ def new_mes(message):
    start_fun(message)
 
 
-@bot.message_handler(content_types="web_app_data") #получаем отправленные данные 
+@bot.message_handler(content_types="web_app_data") #получаем отправленные данные
 def answer(webAppMes):
    print(webAppMes) #вся информация о сообщении
    print(webAppMes.web_app_data.data) #конкретно то что мы передали в бота
-   bot.send_message(webAppMes.chat.id, f"получили инофрмацию из веб-приложения: {webAppMes.web_app_data.data}") 
-   #отправляем сообщение в ответ на отправку данных из веб-приложения 
+   bot.send_message(webAppMes.chat.id, f"получили инофрмацию из веб-приложения: {webAppMes.web_app_data.data}")
+   #отправляем сообщение в ответ на отправку данных из веб-приложения
 
 if __name__ == '__main__':
    bot.infinity_polling()
